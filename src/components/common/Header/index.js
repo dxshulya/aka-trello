535eda34b986ea83c94cce04aa21b2f365e25ca8
dxshulya <daraonelove@yandex.ru>
2022-07-15 15:58:40 +0300

    :)

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import "./style.scss";
+import {MAIN_LINK} from "../../../router/links";
 
 function Header() {
 
     return (
         <header className="header">
-            туту
+            <a href={MAIN_LINK} className="header__smile">☺</a>
         </header>
     );
 }
