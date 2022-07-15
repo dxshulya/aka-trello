65c072d283e7a69c25a5fed137bc777869b6934f
dxshulya <daraonelove@yandex.ru>
2022-07-15 17:06:33 +0300

    todo

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./style.scss";
 import {MainLayout} from "../../components/layout";
+import {Form} from "../../components/common";
 
 function Main() {
 
+    const [open, setOpen] = useState(false);
+    const onClick = () => setOpen(true)
+
     return (
         <MainLayout>
-            <div>Main</div>
+            <div className="main">
+                <div className="main__body">
+                    <div className="creatorColumn">
+                        <div onClick={onClick} className="creator">
+                            <p className="creator__plus">+</p>
+                            <p className="creator__new">Новая доска</p>
+                        </div>
+                        <div>
+                            {open ? <Form/> : null}
+                        </div>
+                    </div>
+                    <div className="boards-titles">sdfsdfsd</div>
+                </div>
+            </div>
         </MainLayout>
     );
 }
